005ae838f8f1f6b58095c00fa27627b40bf8f8c6
MF <128943330+w2w2e2wq@users.noreply.github.com>
2024-07-31 11:16:56 +0800

    a

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/',
           editUrl:
-            'https://github.com/postyizhan/Wiki-Template/blob/main'
+            'https://github.com/w2w2e2wq/wiki/blob/main'
         },
         blog: false,
         theme: {
